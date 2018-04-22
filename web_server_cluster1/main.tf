57bc570b0a5def8f08bdc8e4d77e209910e77563
Harsha Narayana <itsharshanarayana@gmail.com>
2018-04-22 10:44:24 -0400

    Changes to clusters of web servers

--- web_server_cluster1/main.tf
+++ web_server_cluster1/main.tf
@@ -14,6 +14,13 @@ resource "aws_security_group" "test_sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     tags {
         Name = "Test Security Group"
     }
@@ -22,6 +29,18 @@ resource "aws_security_group" "test_sg" {
         create_before_destroy = true
     }
 }
+# Security group for ELB
+resource "aws_security_group" "test_sg_elb" {
+    name = "Test-SG-ELB"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 # Launch configuration resource. 
 resource "aws_launch_configuration" "test_launch_conf" {
     image_id        = "ami-6dfe5010"
@@ -40,9 +59,12 @@ resource "aws_launch_configuration" "test_launch_conf" {
 # Auto scaling group resource.
 resource "aws_autoscaling_group" "test_asg" {
     launch_configuration = "${aws_launch_configuration.test_launch_conf.id}"
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
     min_size = 2
     max_size = 4
 
+    load_balancers = ["${aws_elb.test_load_balancer.name}"]
+    health_check_type = "ELB"
     tag {
         key                 = "Name"
         value               = "Test-Autoscaling-group"
@@ -50,8 +72,32 @@ resource "aws_autoscaling_group" "test_asg" {
     }
 }
 
+resource "aws_elb" "test_load_balancer" {
+    availability_zones = ["${data.aws_availability_zones.all.names}"]
+    security_groups = ["${aws_security_group.test_sg_elb.id}"]
+    listener {
+        lb_port = 80
+        lb_protocol = "http"
+        instance_port = "${var.server_port}"
+        instance_protocol = "http"
+    }
+    health_check {
+        healthy_threshold = 3
+        unhealthy_threshold = 3
+        timeout = 3
+        interval = 30
+        target = "HTTP:${var.server_port}/"
+    }
+}
+
+data "aws_availability_zones" "all" {}
+
 # Variable declarations
 variable "server_port" {
     description = "Port on which server listens"
     default     = 8080
+}
+
+output "public_dns_name" {
+    value = "${aws_elb.test_load_balancer.dns_name}"
 }
